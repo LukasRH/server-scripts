1d1ab372ef90b6be20fe4594a5ef10ee865affd3
LukeRH <lukasronsholt@gmail.com>
2020-05-08 01:54:10 +0200

    test

---
+++ MoveMedia.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+""" TODO make explanation here """
+
+import sys
+
+__author__ = "Lukas Rønsholt"
+__credits__ = []
+__license__ = "MIT"
+__version__ = "0.0.1"
+__maintainer__ = "Lukas Rønsholt"
+__email__ = "lukasronsholt@gmail.com"
+__status__ = "Development"
+
+
+def main(argv):
+    print(str(argv))
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
